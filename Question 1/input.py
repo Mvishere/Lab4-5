44db9f2187f702d39cf81187543abff80b3bfeb2
Manas <isheremv@gmail.com>
2025-02-03 22:13:53 +0530

    initial commit

---
+++ Question 1/input.py
@@ -0,0 +1,20 @@
+def take_input():
+    with open("input.txt", "r") as input_file:
+        row, column = map(int, input_file.readline().strip().split(","))
+        start, end = input_file.readline().strip().split(";")
+        blocked = input_file.readline().strip().split(";")
+        algorithm = input_file.readline().strip()
+
+        matrix = [[0]*column for i in range(row)]
+        
+        start_i, start_j = map(int, start.split(","))
+        matrix[start_i-1][start_j-1] = 1
+
+        end_i, end_j = map(int, end.split(","))
+        matrix[end_i-1][end_j-1] = 2
+
+        for coordinate in blocked:
+            coor_i, coor_j = map(int, coordinate.split(","))
+            matrix[coor_i-1][coor_j-1] = -1
+
+        return [row, column, (start_i-1, start_j-1), (end_i-1, end_j-1), matrix, algorithm]
---
+++ Question 1/main.py
@@ -0,0 +1,8 @@
+from input import take_input
+
+def main():
+    row, column, start, end, matrix, algorithm = take_input()
+    print(row, column, start, end, matrix, algorithm)
+
+if __name__ == "__main__":
+    main()
